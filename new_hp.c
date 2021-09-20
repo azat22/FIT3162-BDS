bc8be7ddbb3aacbff91aa2ec6371cfdb9657edae
Azat <amoh0087@student.monash.edu>
2021-09-20 22:55:02 +1000

    Fixed deleting list entry error

--- new_hp.c
+++ new_hp.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #define NEW_LIST_ENTRY_EVENT 3
 #define NEW_LIST_ENTRY_INPUT 4
@@ -19,11 +20,21 @@ int counter = 1;
 
 hipe_session session;
 
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+    // in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
 hipe_loc getLoc(char* id) {
     hipe_send(session, HIPE_OP_GET_BY_ID, 0, 0, 1, id);
     hipe_instruction instruction;
     hipe_instruction_init(&instruction);
     hipe_await_instruction(session, &instruction, HIPE_OP_LOCATION_RETURN);
+    //We have changed this, it was returning instruction.location
     return instruction.location;
 }
 
@@ -36,33 +47,47 @@ void newListEntryInput(hipe_session session) {
     hipe_instruction_init(&listenForInput);
     
     if(hipe_await_instruction(session, &listenForInput, HIPE_OP_DIALOG_RETURN) == 1) {
-        // div
-        hipe_send(session, HIPE_OP_APPEND_TAG, 0,0, 2, "div", "entryDiv");
-        hipe_loc entryDivLoc = getLoc("entryDiv");
+        if(listenForInput.arg[0] != '\0') {
+            // If the content of the user entry is not empty, then we add it to the list
+            // div
+            char entryNumber[50];
+            sprintf(entryNumber, "%d", counter);
+            char* uniqueEntryDivID = concat("entryDivID", entryNumber);
 
-        hipe_send(session, HIPE_OP_APPEND_TAG, 0,entryDivLoc, 1, "hr");
+            hipe_send(session, HIPE_OP_APPEND_TAG, 0,0, 2, "div", uniqueEntryDivID);
+            hipe_loc entryDivLoc = getLoc(uniqueEntryDivID);
+            hipe_send(session, HIPE_OP_APPEND_TAG, 0, entryDivLoc, 1, "hr");
+            hipe_send(session, HIPE_OP_APPEND_TEXT, 0, entryDivLoc, 2, entryNumber);
+            hipe_send(session, HIPE_OP_APPEND_TEXT, 0, entryDivLoc, 2, ". ");
+            hipe_send(session, HIPE_OP_APPEND_TEXT, 0, entryDivLoc, 2, listenForInput.arg[0]);
 
-        char number[50];
-        sprintf(number, "%d", counter);
-        hipe_send(session, HIPE_OP_APPEND_TEXT, 0,entryDivLoc, 2, number);
-        counter ++;
-        hipe_send(session, HIPE_OP_APPEND_TEXT, 0,entryDivLoc, 2, ".  ");
+            // Create a unique ID for each button
+            char* uniqueButtonID = concat("buttonID", entryNumber);
+            hipe_send(session, HIPE_OP_APPEND_TAG, 0, entryDivLoc, 3, "button", uniqueButtonID, uniqueEntryDivID);
+            
+            
+            
+            hipe_loc deleteButton = getLoc(uniqueButtonID);
+            //add text to the buttons
+            hipe_send(session, HIPE_OP_APPEND_TEXT, 0, deleteButton, 1, "Delete entry");
+            //requests events for these buttons (we designate requestor codes 1,2,3 to identify these quickly)
+            hipe_send(session, HIPE_OP_EVENT_REQUEST, NEW_LIST_DELETE_EVENT, deleteButton, 2, "click", uniqueEntryDivID);
 
-
-        hipe_send(session, HIPE_OP_APPEND_TEXT, 0,entryDivLoc, 2, listenForInput.arg[0]);
-        hipe_send(session, HIPE_OP_APPEND_TAG, 0,entryDivLoc, 2, "button", "deleteEntryButton");
-
-        hipe_loc deleteButton = getLoc("deleteEntryButton");
-
-        //add text to the buttons
-        hipe_send(session, HIPE_OP_APPEND_TEXT, 0,deleteButton, 1, "Delete entry");
-
-        //requests events for these buttons (we designate requestor codes 1,2,3 to identify these quickly)
-        hipe_send(session, HIPE_OP_EVENT_REQUEST, NEW_LIST_DELETE_EVENT, deleteButton, 1, "click");
-
+            counter++;
+        }
     }
 }
 
+void deleteListEntry(hipe_session session) {
+    hipe_instruction listenForDeleteEvent;
+    hipe_instruction_init(&listenForDeleteEvent);
+    
+    if(hipe_await_instruction(session, &listenForDeleteEvent, HIPE_OP_EVENT) == 1) {
+        hipe_loc deleteLocDiv = listenForDeleteEvent.location - 1;
+        hipe_send(session, HIPE_OP_GET_ATTRIBUTE, 0, deleteLocDiv, 0);
+        hipe_send(session, HIPE_OP_DELETE, 0, deleteLocDiv, 2, "button", "deletedeleteButton");
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -71,7 +96,7 @@ int main(int argc, char** argv)
     if(!session) exit(1);
 
     //Create and place buttons
-    hipe_send(session, HIPE_OP_APPEND_TAG, 0,0, 1, "hr"); //horizontal line
+    //hipe_send(session, HIPE_OP_APPEND_TAG, 0,0, 1, "hr"); //horizontal line
 
     hipe_send(session, HIPE_OP_APPEND_TAG, 0,0, 2, "h1", "main-page-title");
     hipe_loc main_page_title_loc = getLoc("main-page-title");
@@ -80,7 +105,7 @@ int main(int argc, char** argv)
     
     hipe_send(session, HIPE_OP_APPEND_TAG, 0,0, 2, "div", "newListEntryDialogButtonDiv");
     hipe_loc newListEntryDialogButtonDivLoc = getLoc("newListEntryDialogButtonDiv");
-    hipe_send(session, HIPE_OP_APPEND_TAG, 0,newListEntryDialogButtonDivLoc, 2, "button", "newListEntryDialogButton");
+    hipe_send(session, HIPE_OP_APPEND_TAG, 0, newListEntryDialogButtonDivLoc, 2, "button", "newListEntryDialogButton");
 
 
     // hipe_send(session, HIPE_OP_APPEND_TAG, 0,0, 2, "div", "newListEntryDialogButtonDiv2");
@@ -100,7 +125,7 @@ int main(int argc, char** argv)
 
     //requests events for these buttons (we designate requestor codes 1,2,3 to identify these quickly)
     hipe_send(session, HIPE_OP_EVENT_REQUEST, NEW_LIST_ENTRY_EVENT, newListEntryDialogButton, 1, "click");
-
+    
     hipe_instruction event;
     hipe_instruction_init(&event);
     
@@ -116,9 +141,9 @@ int main(int argc, char** argv)
 
             case NEW_LIST_DELETE_EVENT:
                     {
-                        hipe_loc deleteLocDiv = getLoc("entryDiv");
-                        hipe_send(session, HIPE_OP_DELETE, 0, deleteLocDiv, 2, "button", "deletedeleteButton");
-
+                        //hipe_loc deleteLocDiv = getLoc("entryDiv");
+                        //hipe_send(session, HIPE_OP_DELETE, 0, deleteLocDiv, 2, "button", "deletedeleteButton");
+                        deleteListEntry(session);
                         // no idea how to find the location of the text itself???
                         // hipe_loc textLoc = getLoc("hr");
                         // hipe_send(session, HIPE_OP_DELETE, 0, textLoc, 2, "text", "deletetext");
@@ -131,3 +156,5 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+
